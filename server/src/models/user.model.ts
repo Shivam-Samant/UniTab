75fa2ea1c5acef091590d2188c455b98b3c49124
Shivam-Samant <shivamsamant000@gmail.com>
2024-12-11 18:23:57 +0530

    code cleanups

--- server/src/models/user.model.ts
+++ server/src/models/user.model.ts
@@ -4,7 +4,6 @@ export interface IUser extends Document {
   name: string;
   email: string;
   authToken: string;
-  activeApp?: { appId: string; tabId: string };
 }
 
 const UserSchema: Schema = new Schema(
@@ -12,10 +11,6 @@ const UserSchema: Schema = new Schema(
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true, index: true },
     authToken: { type: String, required: true, unique: true, index: true },
-    activeApp: {
-      appId: { type: String },
-      tabId: { type: String },
-    },
   },
   {
     timestamps: true,
